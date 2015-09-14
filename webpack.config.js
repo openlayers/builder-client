3a2a5ae6cdb39f6233142646a506ef943f1bd79c
Tim Schaub <tim.schaub@gmail.com>
2015-09-14 09:15:45 +0900

    Add a search box

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,9 @@ module.exports = {
       loaders: ['babel'],
       exclude: /node_modules/,
       include: __dirname
+    }, {
+      test: /\.less$/,
+      loader: 'style!css!less'
     }]
   }
 };
